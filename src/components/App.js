656bbec94fb9bf93d15e02f8afca80ba505de728
Juliya Koltsova <juliyaasking009@gmail.com>
2023-05-24 08:05:17 +0200

    fix: правка после ревью №2

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import './index.css';
 import React from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import PopupWithForm from './components/PopupWithForm';
-import ImagePopup from './components/ImagePopup';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
+import ImagePopup from './ImagePopup';
 
 function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false);
